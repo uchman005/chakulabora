2b1373b43b77f7fd37dbb973c1992a906d20c42a
Marcel Obiefuna <obiefunamarcel@gmail.com>
2023-04-02 09:40:25 +0100

    Fixing signin

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -4,27 +4,17 @@ import Navbar from '@/components/Navbar';
 import { signIn, useSession } from 'next-auth/react';
 import Router from 'next/router'
 import {
-  Flex,
-  Box,
-  FormControl,
-  FormLabel,
-  Input,
   Checkbox,
-  Stack,
-  Link,
-  Button,
-  Heading,
-  Text,
   useColorModeValue,
   useToast
 } from '@chakra-ui/react';
-
+import Link from 'next/link';
 export default function SignIn() {
   const { status } = useSession();
   useEffect(() => {
-      if (status === "authenticated") {
-          Router.replace("/dashboard")
-      }
+    if (status === "authenticated") {
+      Router.replace("/dashboard")
+    }
   }, [status]);
   const [formData, setFormData] = useState({
     email: '',
@@ -69,57 +59,75 @@ export default function SignIn() {
     let { value, name } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }))
   }
+  const bgColor = useColorModeValue("gray.50", "gray.800");
+  const bgColor1 = useColorModeValue("white", "gray.700");
   return (
     <>
       <Navbar />
-      <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
-        bg={useColorModeValue('gray.50', 'gray.800')}>
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-          <Stack align={'center'}>
-            <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-            <Text fontSize={'lg'} color={'gray.600'}>
-              to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
-            </Text>
-          </Stack>
-          <Box
-            rounded={'lg'}
-            bg={useColorModeValue('white', 'gray.700')}
-            boxShadow={'lg'}
-            p={8}>
-            <Stack spacing={4}>
-              <FormControl id="email">
-                <FormLabel>Email address</FormLabel>
-                <Input type="email" placeholder='Enter Your email' name='email' value={formData.email} onChange={handleChange} />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel>Password</FormLabel>
-                <Input type="password" placeholder='* * * *' name='password' value={formData.password} onChange={handleChange} />
-              </FormControl>
-              <Stack spacing={10}>
-                <Stack
-                  direction={{ base: 'column', sm: 'row' }}
-                  align={'start'}
-                  justify={'space-between'}>
-                  <Checkbox>Remember me</Checkbox>
-                  <Link color={'blue.400'}>Forgot password?</Link>
-                </Stack>
-                <Button
-                  bg={'blue.400'}
-                  color={'white'}
-                  _hover={{
-                    bg: 'blue.500',
-                  }}
+      <div className={`flex items-center justify-center min-h-screen bg-${bgColor}`}>
+        <div className="mx-auto max-w-lg py-12 px-6 space-y-8">
+          <div className='items-center'>
+            <h1 className='text-4xl'>Sign in to your account</h1>
+            <span className='text-lg text-gray-600'>
+              to enjoy all of our cool <span className='text-blue-400'>features</span> ✌️
+            </span>
+          </div>
+          <div className={`rounded-lg bg-${bgColor1} shadow-lg p-8`}>
+            <div className='space-4'>
+              <div className="form-froup mb-4">
+                <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+                  Email address
+                </label>
+                <input
+                  className="form-control shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Enter Your email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form-group mb-4">
+                <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="form-control shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="* * * *"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className=''>
+                <div className='flex flex-col sm:flex-row items-start justify-between mb-4'>
+                  <div>
+
+                    <input
+                      type="checkbox"
+                      id="rememberMe"
+                      name="rememberMe"
+                    />
+                    <label className="text-sm" htmlFor="rememberMe">
+                      Remember me
+                    </label>
+                  </div>
+
+                  <Link className='text-blue-400' href={'#'}>Forgot password?</Link>
+                </div>
+                <button
+                  className='text-white bg-blue-400 rounded p-2 hover:bg-blue-500'
                   onClick={handleClick}>
                   Sign in
-                </Button>
-              </Stack>
-            </Stack>
-          </Box>
-        </Stack>
-      </Flex>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
